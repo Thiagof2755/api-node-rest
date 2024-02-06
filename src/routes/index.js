ccc279ffd84da5c20e10113c449fc281ecaddd67
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-02-06 19:54:32 -0300

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,17 @@ import livro from "./LivrosRouters.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
-        res.status(200).send({ titulo: "Api de livros", version: "1.0.0" });
+        res.status(200).send({ 
+            titulo: "Api de livros", 
+            version: "1.0.0",
+            autor: "Thiago Alves ",
+            descricao: "Uma API para gerenciar livros no MongoDB",
+            comoUsar: "Consulte a documentação para obter instruções sobre como usar a API",
+            GitHub_Documentação: "https://github.com/Thiagof2755/api-node-rest"
+        });
     })
 
-    app.use(express.json(),livro);
-    
+    app.use(express.json(), livro);
 };
 
 export default routes;
